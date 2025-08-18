72b3380da4050510951318275e3e3445f3e7dc7b
caliskankubra <kubra.clskn94@gmail.com>
2025-08-18 23:09:41 +0300

    feat: initialize loan-api project

---
+++ src/main/java/com/ing/hubs/ing/hubs/loan/api/IngHubsLoanApiApplication.java
@@ -0,0 +1,13 @@
+package com.ing.hubs.ing.hubs.loan.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngHubsLoanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngHubsLoanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/hubs/ing/hubs/loan/api/IngHubsLoanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.hubs.ing.hubs.loan.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngHubsLoanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
